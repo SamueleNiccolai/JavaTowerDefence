335d6969f6d57c602c8a0a498eb57feda2485b55
Samuele Niccolai <samuniccolai@gmail.com>
2024-03-30 18:51:29 +0100

    Tutorial 2: how to get object from the image

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,27 +1,64 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
 	
 	private GameScreen gameScreen;
+	private BufferedImage img;
+	
+	public GameScreen getGameScreen() {
+		return gameScreen;
+	}
+	
+	public void setGameScreen(GameScreen gameScreen) {
+		this.gameScreen = gameScreen;
+	}
+	
+	public BufferedImage getImg() {
+		return img;
+	}
+
+	public void setImg(BufferedImage img) {
+		this.img = img;
+	}
+	
 	public Game() {
+		
+		importImg();
+		
 		// To Create a Window
-		setSize(400,400);
+		setSize(640, 640);
 		setVisible(true);
+		
+			// To stop the program on window close
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		// To open the position in the center
 		setLocationRelativeTo(null);
 		
 		// To add object
-		this.gameScreen = new GameScreen();
+		setGameScreen(new GameScreen(getImg()));
 		add(gameScreen);
+	}
+
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/spriteatlas.png"); // Browse all folders to find a resource named like this
+		try {
+			setImg(ImageIO.read(is));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public static void main(String[] args) {
 		Game game = new Game();
 	}
-	
 
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,21 +2,83 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class GameScreen extends JPanel{
+public class GameScreen extends JPanel {
 
-	public GameScreen() {
-		
+	private Random random;
+	private BufferedImage img;
+	// private HashMap<String, BufferedImage> sprites = new HashMap<String,
+	// BufferedImage>();
+	private ArrayList<BufferedImage> spriteList = new ArrayList<BufferedImage>();
+
+	public BufferedImage getImg() {
+		return img;
 	}
-	
+
+	public void setImg(BufferedImage img) {
+		this.img = img;
+
+	}
+
+//	public HashMap<String, BufferedImage> getSprites() {
+//		return sprites;
+//	}
+//
+//	public void setSprites(HashMap<String, BufferedImage> sprites) {
+//		this.sprites = sprites;
+//	}
+
+	public ArrayList<BufferedImage> getSpriteList() {
+		return spriteList;
+	}
+
+	public void setSpriteList(ArrayList<BufferedImage> spriteList) {
+		this.spriteList = spriteList;
+	}
+
+	public GameScreen(BufferedImage img) {
+		random = new Random();
+		setImg(img);
+		loadSprites();
+	}
+
+	private void loadSprites() {
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				getSpriteList().add(getImg().getSubimage(x * 32, y * 32, 32, 32));
+			}
+		}
+	}
+
 	// To call the super class to do all the calculation
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		g.setColor(Color.RED);
-		g.drawRect(50, 50, 100, 100);
+		// to draw the full img
+		// g.drawImage(getImg(), 0, 0, null);
+
+		g.drawImage(getSpriteList().get(91), 0, 0, null);
+
+		for (int x = 0; x < 20; x++) {
+			for (int y = 0; y < 20; y++) {
+				g.drawImage(getSpriteList().get(getRndInt(100)), x * 32, y * 32, null);
+			}
+		}
+	}
+
+	private Color getRndColor() {
+		int red = random.nextInt(256); // => 0 <= red < 256
+		int green = random.nextInt(256);
+		int blue = random.nextInt(256);
+		return new Color(red, green, blue);
+	}
+
+	private int getRndInt(int MaxValueExcluded) {
+		return random.nextInt(MaxValueExcluded);
 	}
 
 }
